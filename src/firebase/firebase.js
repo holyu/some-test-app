917b0f5df8df3e2ce2157b1c77945ed1c0f712ec
Piotr Gorski <holyu@veganka.pl>
2018-05-03 22:06:38 +0200

    ENV uppercase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,12 +7,12 @@ import "firebase/database";
 
 // Initialize Firebase
 const config = {
-  apiKey: process.env.FIREBASE_apiKey,
-  authDomain: process.env.FIREBASE_authDomain,
-  databaseURL: process.env.FIREBASE_databaseURL,
-  projectId: process.env.FIREBASE_projectId,
-  storageBucket: process.env.FIREBASE_storageBucket,
-  messagingSenderId: process.env.FIREBASE_messagingSenderId
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STOREGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
